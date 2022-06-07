86b2018291ade47b40c1f763bdfb874c8bf7fcdc
Danil <93711640+p1ck234@users.noreply.github.com>
2022-06-07 20:03:39 +0400

    Update UnitTest1.cpp
    доработал тесты

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -9,56 +9,56 @@ namespace UnitTests
 	TEST_CLASS(Tests)
 	{
 	public:
-
-		TEST_METHOD(Normal_Input) // Нормальный набор
+		TEST_METHOD(Normal_Input) // РќРѕСЂРјР°Р»СЊРЅС‹Р№ РЅР°Р±РѕСЂ
 		{
 			int arr[10]{ 56,34355,345,345,34534,5346,8,5465,113,119986 };
-			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 1);
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 5);
 		}
-		TEST_METHOD(Null_Input) // Нулевой набор 
+		TEST_METHOD(Null_Input) // РќСѓР»РµРІРѕР№ РЅР°Р±РѕСЂ 
 		{
-			int arr[1]{ };
+			int arr[1]{ 0 };
 			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(1, arr), 0);
 		}
-		TEST_METHOD(One_Input) // Единичный набор 
+		TEST_METHOD(One_Input) // Р•РґРёРЅРёС‡РЅС‹Р№ РЅР°Р±РѕСЂ 
 		{
 			int arr[1]{ 1 };
 			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(1, arr), 0);
 		}
-		TEST_METHOD(Repeat_Number_Input) // Набор с повторяющимися элементами 
+		TEST_METHOD(Repeat_Number_Input) // РќР°Р±РѕСЂ СЃ РїРѕРІС‚РѕСЂСЏСЋС‰РёРјРёСЃСЏ СЌР»РµРјРµРЅС‚Р°РјРё 
 		{
 			int arr[5]{ 11,23,10,1,1 };
-			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(5, arr), 0);
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(5, arr), 1);
 		}
-		TEST_METHOD(Otr_Number_Input) // Набор с отрицательными элементами 
+		TEST_METHOD(Otr_Number_Input) // РќР°Р±РѕСЂ СЃ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹РјРё СЌР»РµРјРµРЅС‚Р°РјРё 
 		{
 			int arr[5]{ 11,-3,10,33,-1 };
-			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(5, arr), 0);
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(5, arr), 2);
 		}
-		TEST_METHOD(Otr_Repeat_Number_Input) // Набор с повторяющимися отрицательными элементами 
+		TEST_METHOD(Otr_Repeat_Number_Input) // РќР°Р±РѕСЂ СЃ РїРѕРІС‚РѕСЂСЏСЋС‰РёРјРёСЃСЏ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹РјРё СЌР»РµРјРµРЅС‚Р°РјРё 
 		{
-			int arr[10]{ 434,6554,5445,11,-1,10,33,-1,353,4344 };
-			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 9);
+			int arr[10]{ 434,6574,5445,11,-1,10,33,-1,353,4344 };
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 1);
 		}
-		TEST_METHOD(Ectrem_Input_Begin) // Экстремальное значение находиться в начале 
+		TEST_METHOD(Ectrem_Input_Begin) // Р­РєСЃС‚СЂРµРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РЅР°С…РѕРґРёС‚СЊСЃСЏ РІ РЅР°С‡Р°Р»Рµ 
 		{
 			int arr[10]{ 9,6554,5445,11,15,10,33,10,353 };
 			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 1);
 		}
-		TEST_METHOD(Ectrem_Input_Center) // Экстремальное значение находиться в центре 
+		TEST_METHOD(Ectrem_Input_Center) // Р­РєСЃС‚СЂРµРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РЅР°С…РѕРґРёС‚СЊСЃСЏ РІ С†РµРЅС‚СЂРµ 
 		{
 			int arr[10]{ 115653,6554,5445,9,15,10,33,10,353 };
 			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 0);
 		}
-		TEST_METHOD(Ectrem_Input_End) // Экстремальное значение находиться в конце 
+		TEST_METHOD(Ectrem_Input_End) // Р­РєСЃС‚СЂРµРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РЅР°С…РѕРґРёС‚СЊСЃСЏ РІ РєРѕРЅС†Рµ 
 		{
-			int arr[10]{ 643,654,654,363,150,643,643,104,9 };
-			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 3);
+			int arr[10]{ 676,656,656,363,155,646,644,404,111,654 };
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 9);
 		}
-		TEST_METHOD(Ectrem_Repeat_Input) // Экстремальное значения повторяются 
+		TEST_METHOD(Ectrem_Repeat_Input) // Р­РєСЃС‚СЂРµРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёСЏ РїРѕРІС‚РѕСЂСЏСЋС‚СЃСЏ 
 		{
-			int arr[10]{ 9,9,543,545,150,643,643,104,103 };
-			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 3);
+			int arr[10]{ 9,9,543,545,150,6475,6475,104,103 };
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(func(10, arr), 5);
 		}
 	};
 }
+
