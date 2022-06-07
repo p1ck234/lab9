65dfba405f8d3849203cf569d95f612f149a5b6f
Danil <93711640+p1ck234@users.noreply.github.com>
2022-06-07 20:01:56 +0400

    Update lab9.cpp
    исправил баги

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,34 +1,38 @@
 #include "pch.h"
 
-int kol(int x, int a[10] = { 0 }) {
-	int i = 0;
+int kol(int x) {
+	int a[10] = { 0 };
+
 	while (x > 0) {
 		a[x % 10]++;
 		x /= 10;
-		i++;
 	}
-	return i;
+	int k = 0;
+	for (int j = 0; j < 10; j++)
+		if (a[j] != 0) k++;
+
+
+
+	return k;
 }
 
 int func(int n, int arr[]) {
-	int counter = 0;
-	int max = 1, m, temp = 0;
+	int _id = 0;
+	int max = 1;
 	for (int i = 0; i < n; i++)
 	{
-		int a[10] = { 0 };
-		m = kol(arr[i], a);
-		for (int j = 0; j < 10; j++)
-			if (max < a[j])
-			{
-				max = a[j];
-				temp = i;
-			}
+
+		if (kol(arr[i]) > max) {
+			max = kol(arr[i]); _id = i;
+		}
+		
 	}
 
-	return temp;
+	return _id;
 }
 
 int main()
 {
 	return 0;
 }
+
